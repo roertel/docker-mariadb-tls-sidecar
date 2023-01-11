df59dd2cb2978dcbfd33d902cb98d7148be1606d
roertel <ryan.oertel@gmail.com>
2023-01-11 14:40:44 -0600

    Added entrypoint script debug feature (#3)
    * Initial attempt.
    
    * updated build to push to GHCR
    
    * fixed variable name
    
    * changed to GHCR
    
    * base image from ghcr.io
    
    * found a few registry references to docker.io
    
    * Another attempt
    
    * updated versions of actions
    
    * added permissions check.
    
    * Added entrypoint debugging feature

--- src/usr/local/bin/entrypoint.sh
+++ src/usr/local/bin/entrypoint.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
+set -e
+
+if [ "$DEBUG" = 'true' ]; then
+  set -x
+fi
 
 # Load secrets into env vars for building config
-test -r /run/secrets/ldap-bindpw &&
+if [ -r /run/secrets/ldap-bindpw ]; then
    export LDAP_BINDPW="$(cat /run/secrets/ldap-bindpw)"
+fi
 
 # Create config from template
-test -r /templates/nslcd.conf.tpl && \
+if [ -r /templates/nslcd.conf.tpl ]; then
    envsubst</templates/nslcd.conf.tpl>/etc/nslcd.conf
+fi
 
 # If command starts with an option, prepend nslcd.
 # This allows users to start another executable. 
